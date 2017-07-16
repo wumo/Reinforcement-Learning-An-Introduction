86c1aed092d66b4f2cb4902af377e734dfa203ab
Hu Haixiao <wumo@outlook.com>
2017-07-16 16:17:49 +0800

    added D2DPossibleSet.

---
+++ src/main/java/wumo/rl/model/impl/D2DPossibleSet.java
@@ -0,0 +1,40 @@
+package wumo.rl.model.impl;
+
+import org.jetbrains.annotations.NotNull;
+import wumo.rl.model.MDP;
+import wumo.rl.util.*;
+
+import java.util.Iterator;
+
+public class D2DPossibleSet implements MDP.PossibleSet {
+    public MDP.Possible[][] raw;
+    
+    @Override public MDP.Possible get(Object index) {
+        tuple2 idx = (tuple2) index;
+        return raw[idx._1][idx._2];
+    }
+    
+    @Override public void set(Object index, MDP.Possible s) {
+        tuple2 idx = (tuple2) index;
+        raw[idx._1][idx._2] = s;
+    }
+    
+    @NotNull @Override public Iterator<MDP.Possible> iterator() {
+        return new Iterator<MDP.Possible>() {
+            int a = 0, b = 0;
+            
+            @Override public boolean hasNext() {
+                return a < raw.length && b < raw[a].length;
+            }
+            
+            @Override public MDP.Possible next() {
+                int _a = a, _b = b;
+                b++;
+                if (b >= raw[a].length) {
+                    a++; b = 0;
+                }
+                return raw[_a][_b];
+            }
+        };
+    }
+}
