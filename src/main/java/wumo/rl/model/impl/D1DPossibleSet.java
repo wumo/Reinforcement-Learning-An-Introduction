b3e2e72c8165cc78563d0c07c6140a9d7fd289c9
Hu Haixiao <wumo@outlook.com>
2017-07-16 16:30:14 +0800

    added D1DPossibleSet;

--- src/main/java/wumo/rl/model/impl/D1DPossibleSet.java
+++ src/main/java/wumo/rl/model/impl/D1DPossibleSet.java
@@ -2,7 +2,6 @@ package wumo.rl.model.impl;
 
 import org.jetbrains.annotations.NotNull;
 import wumo.rl.model.MDP;
-import wumo.rl.util.tuple2;
 
 import java.util.Iterator;
 
@@ -10,37 +9,36 @@ import java.util.Iterator;
  * single state + D1D reward
  */
 public class D1DPossibleSet implements MDP.PossibleSet {
-    public final MDP.Possible[][] raw;
+    public final MDP.Possible[] raw;
     
+    /**
+     * @param n
+     *         reward size
+     */
     public D1DPossibleSet(int n) {
-        this.raw = new MDP.Possible[n][];
+        this.raw = new MDP.Possible[n];
     }
     
     @Override public MDP.Possible get(Object index) {
-        tuple2 idx = (tuple2) index;
-        return raw[idx._1][idx._2];
+        return raw[(int) index];
     }
     
     @Override public void set(Object index, MDP.Possible s) {
-        tuple2 idx = (tuple2) index;
-        raw[idx._1][idx._2] = s;
+        raw[(int) index] = s;
     }
     
     @NotNull @Override public Iterator<MDP.Possible> iterator() {
         return new Iterator<MDP.Possible>() {
-            int a = 0, b = 0;
+            int a = 0;
             
             @Override public boolean hasNext() {
-                return a < raw.length && b < raw[a].length;
+                return a < raw.length;
             }
             
             @Override public MDP.Possible next() {
-                int _a = a, _b = b;
-                b++;
-                if (b >= raw[a].length) {
-                    a++; b = 0;
-                }
-                return raw[_a][_b];
+                int _a = a;
+                a++;
+                return raw[_a];
             }
         };
     }
