b030ab2b6fefb5fb73d77013b4d620021e6b42a2
Hu Haixiao <wumo@outlook.com>
2017-08-31 19:13:36 +0800

    update

--- src/main/java/wumo/rl/model/impl/D1DStateSet.java
+++ src/main/java/wumo/rl/model/impl/D1DStateSet.java
@@ -1,33 +1,52 @@
 package wumo.rl.model.impl;
 
 import wumo.rl.model.MDP;
+import wumo.rl.model.MDP.State;
 
 import java.util.Iterator;
+import java.util.function.*;
 
 public class D1DStateSet implements MDP.StateSet {
-    public final MDP.State[] raw;
-    
+    public final State[] raw;
+
     public D1DStateSet(int d1dstate_dim) {
-        raw = new MDP.State[d1dstate_dim];
+        this(d1dstate_dim, State::new);
     }
-    
-    @Override public MDP.State get(Object index) {
+
+    public D1DStateSet(int d1dstate_dim, Supplier<? extends State> state_maker) {
+        raw = new State[d1dstate_dim];
+        for (int a = 0; a < d1dstate_dim; a++)
+            raw[a] = state_maker.get();
+    }
+
+    public D1DStateSet(int d1dstate_dim, Function<Integer, ? extends State> state_maker) {
+        raw = new State[d1dstate_dim];
+        for (int a = 0; a < d1dstate_dim; a++)
+            raw[a] = state_maker.apply(a);
+    }
+
+    @Override
+    public State get(Object index) {
         return raw[(int) index];
     }
-    
-    @Override public void set(Object index, MDP.State s) {
+
+    @Override
+    public void set(Object index, State s) {
         raw[(int) index] = s;
     }
-    
-    @Override public Iterator<MDP.State> iterator() {
-        return new Iterator<MDP.State>() {
+
+    @Override
+    public Iterator<State> iterator() {
+        return new Iterator<State>() {
             int a = 0;
-            
-            @Override public boolean hasNext() {
+
+            @Override
+            public boolean hasNext() {
                 return a < raw.length;
             }
-            
-            @Override public MDP.State next() {
+
+            @Override
+            public State next() {
                 int _a = a;
                 a++;
                 return raw[_a];
--- src/main/java/wumo/rl/model/impl/D2DStateSet.java
+++ src/main/java/wumo/rl/model/impl/D2DStateSet.java
@@ -2,36 +2,58 @@ package wumo.rl.model.impl;
 
 import org.jetbrains.annotations.NotNull;
 import wumo.rl.model.MDP;
+import wumo.rl.model.MDP.State;
 import wumo.rl.util.index2;
 
 import java.util.Iterator;
+import java.util.function.*;
 
 public class D2DStateSet implements MDP.StateSet {
-    public final MDP.State[][] raw;
-    
+    public final State[][] raw;
+
     public D2DStateSet(int d2dstate_first_dim, int d2dstate_second_dim) {
-        raw = new MDP.State[d2dstate_first_dim][d2dstate_second_dim];
+        this(d2dstate_first_dim, d2dstate_second_dim, State::new);
     }
-    
-    @Override public MDP.State get(Object index) {
+
+    public D2DStateSet(int d2dstate_first_dim, int d2dstate_second_dim, Supplier<? extends State> state_maker) {
+        raw = new State[d2dstate_first_dim][d2dstate_second_dim];
+        for (int a = 0; a < d2dstate_first_dim; a++)
+            for (int b = 0; b < d2dstate_second_dim; b++)
+                raw[a][b] = state_maker.get();
+    }
+
+    public D2DStateSet(int d2dstate_first_dim, int d2dstate_second_dim, BiFunction<Integer, Integer, ? extends State> state_maker) {
+        raw = new State[d2dstate_first_dim][d2dstate_second_dim];
+        for (int a = 0; a < d2dstate_first_dim; a++)
+            for (int b = 0; b < d2dstate_second_dim; b++)
+                raw[a][b] = state_maker.apply(a, b);
+    }
+
+    @Override
+    public State get(Object index) {
         index2 idx = (index2) index;
         return raw[idx._1][idx._2];
     }
-    
-    @Override public void set(Object index, MDP.State s) {
+
+    @Override
+    public void set(Object index, State s) {
         index2 idx = (index2) index;
         raw[idx._1][idx._2] = s;
     }
-    
-    @NotNull @Override public Iterator<MDP.State> iterator() {
-        return new Iterator<MDP.State>() {
+
+    @NotNull
+    @Override
+    public Iterator<State> iterator() {
+        return new Iterator<State>() {
             int a = 0, b = 0;
-            
-            @Override public boolean hasNext() {
+
+            @Override
+            public boolean hasNext() {
                 return a < raw.length && b < raw[a].length;
             }
-            
-            @Override public MDP.State next() {
+
+            @Override
+            public State next() {
                 int _a = a, _b = b;
                 b++;
                 if (b >= raw[a].length) {
