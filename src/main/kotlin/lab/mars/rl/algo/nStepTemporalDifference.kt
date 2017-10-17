503db2444b28ba0db610813985089f7a4a88cbbb
wumo <wumo@outlook.com>
2017-10-17 18:58:01 +0800

    完成off-policy Q sigma

--- src/main/kotlin/lab/mars/rl/algo/nStepTemporalDifference.kt
+++ src/main/kotlin/lab/mars/rl/algo/nStepTemporalDifference.kt
@@ -217,7 +217,7 @@ class nStepTemporalDifference(val mdp: MDP, val n: Int, private var policy: NonD
     }
 
     fun treebackup(): OptimalSolution {
-        val pi = mdp.QFunc { 0.0 }
+        val pi = policy
         val Q = mdp.QFunc { 0.0 }
         val S = newBuf<State>(min(n, MAX_N))
         val A = newBuf<Action>(min(n, MAX_N))
@@ -272,6 +272,91 @@ class nStepTemporalDifference(val mdp: MDP, val n: Int, private var policy: NonD
                         if (k < end) e *= gamma * tmpPi[k + 1]
                     }
                     Q[S[0], A[0]] += alpha * (G - Q[S[0], A[0]])
+                    updatePolicy(states[S[0]], Q, pi)
+                }
+                t++
+            } while (_t < T - 1)
+            log.debug { "n=$n,T=$T" }
+        }
+        val V = mdp.VFunc { 0.0 }
+        val result = Triple(pi, V, Q)
+        V_from_Q_ND(states, result)
+        return result
+    }
+
+    lateinit var sig: RandomAccessCollection<Int>
+
+    fun `off-policy Q delta`(): OptimalSolution {
+        val b = mdp.QFunc { 1.0 }
+        for (s in states) {
+            if (s.isTerminal()) continue
+            val prob = 1.0 / s.actions.size
+            for (a in s.actions)
+                b[s, a] = prob
+        }
+        val pi = policy
+        val Q = mdp.QFunc { 0.0 }
+        val S = newBuf<State>(min(n, MAX_N))
+        val A = newBuf<Action>(min(n, MAX_N))
+        val delta = newBuf<Double>(min(n, MAX_N))
+        val tmpQ = newBuf<Double>(min(n, MAX_N))
+        val tmpPi = newBuf<Double>(min(n, MAX_N))
+        val tmpP = newBuf<Double>(min(n, MAX_N))
+        val tmpSig = newBuf<Int>(min(n, MAX_N))
+
+        for (episode in 1..episodes) {
+            var n = n
+            log.debug { "$episode/$episodes" }
+            var T = Int.MAX_VALUE
+            var t = 0
+            var s = started.rand()
+            updatePolicy(s, Q, pi)
+            var a = s.actions.rand(b(s))
+            tmpQ.clear(); tmpQ.append(0.0)
+            tmpPi.clear(); tmpPi.append(0.0)
+            tmpP.clear();tmpP.append(0.0)
+            tmpSig.clear(); tmpSig.append(1)
+            S.clear();S.append(s)
+            A.clear();A.append(a)
+            do {
+                if (t >= n) {//最多存储n个
+                    tmpPi.removeFirst(1)
+                    delta.removeFirst(1)
+                    S.removeFirst(1)
+                    A.removeFirst(1)
+                }
+                if (t < T) {
+                    val possible = a.sample()
+                    S.append(possible.next)
+                    s = possible.next
+                    val r = possible.reward
+                    if (s.isTerminal()) {
+                        delta.append(r - tmpQ.last)
+                        T = t + 1
+                        val _t = t - n + 1
+                        if (_t < 0) n = T //n is too large, normalize it
+                    } else {
+                        a = s.actions.rand(b(s));A.append(a)
+                        val sig = sig[t + 1]
+                        tmpSig.append(sig)
+                        delta.append(r + gamma * sig * Q[s, a] + gamma * (1 - sig) * Sigma(s.actions) { pi[s, it] * Q[s, it] } - tmpQ.last)
+                        tmpQ.append(Q[s, a])
+                        tmpPi.append(pi[s, a])
+                        tmpP.append(pi[s, a] / b[s, a])
+                    }
+                }
+                val _t = t - n + 1
+                if (_t >= 0) {
+                    var p = 1.0
+                    var e = 1.0
+                    var G = Q[0]
+                    val end = min(n - 1, T - 1 - _t)
+                    for (k in 0..end) {
+                        G += e * delta[k]
+                        if (k < end) e *= gamma * ((1 - delta[k + 1]) * tmpPi[k + 1] + delta[k + 1])
+                        p *= 1 - delta[k] + delta[k] * tmpP[k]
+                    }
+                    Q[S[0], A[0]] += alpha * p * (G - Q[S[0], A[0]])
                     updatePolicy(states[S[0]], Q, pi)
                 }
                 t++
