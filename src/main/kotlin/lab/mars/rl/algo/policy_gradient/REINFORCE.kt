d47c0cec717801fc207e79b93418b7608e0de38a
wumo <wumo@outlook.com>
2017-12-01 11:22:06 +0800

    refactor

--- src/main/kotlin/lab/mars/rl/algo/policy_gradient/REINFORCE.kt
+++ src/main/kotlin/lab/mars/rl/algo/policy_gradient/REINFORCE.kt
@@ -5,7 +5,7 @@ import lab.mars.rl.algo.func_approx.FunctionApprox.Companion.log
 import lab.mars.rl.model.*
 import lab.mars.rl.model.impl.func.LinearFunc
 import lab.mars.rl.util.buf.newBuf
-import lab.mars.rl.util.debug
+import lab.mars.rl.util.log.debug
 import lab.mars.rl.util.math.rand
 import lab.mars.rl.util.matrix.times
 import lab.mars.rl.util.matrix.Σ
