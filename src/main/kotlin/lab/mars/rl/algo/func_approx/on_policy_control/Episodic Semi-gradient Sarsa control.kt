7bc228808f8b159b09753b09d95ab57b49f9a6fb
wumo <wumo@outlook.com>
2017-12-01 13:01:23 +0800

    added REINFORCE with Baseline (episodic)

--- src/main/kotlin/lab/mars/rl/algo/func_approx/on_policy_control/Episodic Semi-gradient Sarsa control.kt
+++ src/main/kotlin/lab/mars/rl/algo/func_approx/on_policy_control/Episodic Semi-gradient Sarsa control.kt
@@ -3,7 +3,7 @@ package lab.mars.rl.algo.func_approx.on_policy_control
 import lab.mars.rl.algo.func_approx.FunctionApprox
 import lab.mars.rl.algo.func_approx.FunctionApprox.Companion.log
 import lab.mars.rl.model.*
-import lab.mars.rl.util.debug
+import lab.mars.rl.util.log.debug
 import lab.mars.rl.util.matrix.times
 
 fun <E> FunctionApprox.`Episodic semi-gradient Sarsa control`(q: ApproximateFunction<E>, trans: (State, Action<State>) -> E) {
--- src/main/kotlin/lab/mars/rl/algo/policy_gradient/REINFORCE with Baseline (episodic).kt
+++ src/main/kotlin/lab/mars/rl/algo/policy_gradient/REINFORCE with Baseline (episodic).kt
@@ -10,7 +10,9 @@ import lab.mars.rl.util.math.rand
 import lab.mars.rl.util.matrix.times
 import lab.mars.rl.util.matrix.Σ
 
-fun <E> FunctionApprox.REINFORCE(π: ApproximateFunction<E>, trans: (State, Action<State>) -> E) {
+fun <E> FunctionApprox.`REINFORCE with Baseline (episodic)`(
+    π: ApproximateFunction<E>, trans: (State, Action<State>) -> E, α_θ: Double,
+    v: ApproximateFunction<E>, transV: (State) -> E, α_w: Double) {
     for (episode in 1..episodes) {
         log.debug { "$episode/$episodes" }
         var step = 0
@@ -40,12 +42,14 @@ fun <E> FunctionApprox.REINFORCE(π: ApproximateFunction<E>, trans: (State, Acti
         var γ_t = 1 / γ
         for (t in 0 until T) {
             val G = accu - R[t]
+            val δ = G - v(transV(S[t]))
             γ_t *= γ
+            v.w += α_w * γ_t * δ * v.`▽`(transV(S[t]))
             val `▽` = if (π is LinearFunc)
                 π.x(trans(S[t], A[t])) - Σ(S[t].actions) { π(trans(S[t], it)) * π.x(trans(S[t], it)) }
             else
                 π.`▽`(trans(S[t], A[t])) / π(trans(S[t], A[t]))
-            π.w += α * γ_t * G * `▽`
+            π.w += α_θ * γ_t * δ * `▽`
         }
     }
 }
